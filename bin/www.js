19ab853a1b2a16c8e1603e13123aa50e84cc89b5
sirrodgepodge <rwcbeaman@gmail.com>
2015-04-27 19:29:37 -0400

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -5,7 +5,7 @@
  */
 
 var app = require('../app');
-var debug = require('debug')('bawss:herodb');
+var debug = require('debug')('passport-local-express4:server');
 var http = require('http');
 
 /**
